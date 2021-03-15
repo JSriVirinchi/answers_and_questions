6d4799fd0a2b350921280364cc470fae5e9101b6
JSriVirinchi <jsrivirinchi@gmail.com>
2021-03-15 11:34:02 +0530

    updating marks by adding column in the table, fixing question number when deleting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,7 +26,7 @@ class HomeController < ApplicationController
 	# questionpaper
 	def create_question_paper
 		# This posts the question paper into the table
-		@questionpaper = Questionpaper.new(params.require(:create_question_paper).permit(:name).merge(user_id: current_user.id, number_of_questions: 0))
+		@questionpaper = Questionpaper.new(params.require(:create_question_paper).permit(:name).merge(user_id: current_user.id, number_of_questions: 0, total_marks: 0))
 		if @questionpaper.save
 			redirect_to home_question_paper_edit_page_path(@questionpaper.id)
 		else
@@ -66,6 +66,8 @@ class HomeController < ApplicationController
 	# questions
 	def create_question
 		# To create a question
+		# Changes the number of questions in question paper accordingly
+		# Modifies the total marks accordingly
 		@questionpaper = Questionpaper.find(params[:questionpaper_id])
 		question_number = Question.where(questionpaper_id: @questionpaper.id).count + 1
 		default_marks = 2
@@ -75,6 +77,13 @@ class HomeController < ApplicationController
 		number_of_questions = number_of_questions + 1
 		@questionpaper.update(number_of_questions: number_of_questions)
 
+		total_marks = 0 
+		for i in Question.where(questionpaper_id: @questionpaper.id)
+			total_marks = total_marks + i.marks 
+		end
+
+		@questionpaper.update(total_marks: total_marks)
+
 		respond_to do |format|
 		    format.js
 	  	end
@@ -82,14 +91,40 @@ class HomeController < ApplicationController
 
 	def delete_question
 		# delete question from the table
+		# Changes the number of questions in question paper accordingly
+		# Adjusts the question number such that they will be continuous.
+		# Modifies the total marks accordingly
+
 		@question = Question.find(params[:question_id])
 		@questionpaper = Questionpaper.find(@question.questionpaper_id)
 
+		# fixing the continuous question number
+		deleting_question_number = @question.question_number
+
+		for i in Question.where(questionpaper_id: @questionpaper.id)
+			if i.question_number > deleting_question_number
+				new_question_number = i.question_number - 1
+				i.update(question_number: new_question_number)
+			end
+		end
+
+		# fixing number of questions
 		number_of_questions = @questionpaper.number_of_questions
 		number_of_questions = number_of_questions - 1
 		@questionpaper.update(number_of_questions: number_of_questions)
 
+		# deleting the question
 		@question.destroy
+
+		# fixing the total marks
+		total_marks = 0 
+		for i in Question.where(questionpaper_id: @questionpaper.id)
+			total_marks = total_marks + i.marks 
+		end
+
+		@questionpaper.update(total_marks: total_marks)
+
+		# ajax
 		respond_to do |format|
 		    format.js
 	  	end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_163956) do
+ActiveRecord::Schema.define(version: 2021_03_15_051050) do
 
   create_table "questionpapers", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_163956) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.decimal "total_marks"
   end
 
   create_table "questions", force: :cascade do |t|

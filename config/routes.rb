48be66c3616ac1c578bff140b6a9ec20c792fb15
JSriVirinchi <jsrivirinchi@gmail.com>
2021-04-17 21:33:52 +0530

    profile page left pane styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'home#dashboard_page'
   get 'home/question_paper_page', to: 'home#question_paper_page', as: 'home_question_paper_page'
   get 'home/question_paper_edit_page/:questionpaper_id', to: 'home#question_paper_edit_page', as: 'home_question_paper_edit_page'
+  get 'settings/profile_page/:user_id', to: 'settings#profile_page', as:'settings_profile_page'
 
   # General pages
   get 'general/landing_page', to: 'general#landing_page', as: 'general_landing_page'
